695040aa905aa72be83ecc0a4b21383e65bd39e9
PierceLe <haichau.hvsg@gmail.com>
2024-06-24 17:57:02 +0700

    fix: ex2

--- practice-basic/Ex2.java
+++ practice-basic/Ex2.java
@@ -3,17 +3,27 @@ import java.util.*;
 
 public class Ex2 {
     public static void main (String args[]) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Please enter the electricity degree in this month: ");
-        int elecDegree = sc.nextInt();
-        if (elecDegree <= 100) {
-            System.out.println(elecDegree * 1000);
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Nhập số điện đã dùng trong tháng: ");
+        int usage = scanner.nextInt();
+        
+        int bill = calculateElectricityBill(usage);
+        
+        System.out.println("Số tiền điện phải trả: " + bill + " VND");
+    }
+    
+    public static int calculateElectricityBill(int usage) {
+        int bill = 0;
+        
+        if (usage <= 100) {
+            bill = usage * 1000;
+        } else if (usage <= 150) {
+            bill = 100 * 1000 + (usage - 100) * 1500;
+        } else {
+            bill = 100 * 1000 + 50 * 1500 + (usage - 150) * 2000;
         }
-        else if (elecDegree <= 200) {
-            System.out.println(100 * 1000 + (elecDegree - 100) * 1500);
-        }
-        else {
-            System.out.println(100 * 1000 + 100 * 1500 + (elecDegree - 200) * 2000);
-        }
+        
+        return bill;
     }
 }
